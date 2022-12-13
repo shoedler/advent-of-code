f848375c7135a3d56a28a57309935f6272b1deea
Simon Schödler <simon.schoedler@nexplore.ch>
2022-12-13 09:15:28 +0100

    Refactors day 13

--- 2022/typescript/13/solution.ts
+++ 2022/typescript/13/solution.ts
@@ -7,9 +7,9 @@ const packetPairs = fs.readFileSync('./input.txt', 'utf-8').split('\r\n\r\n')
 
 const comparePkgPair = (left: any, right: any): number => {
   if (typeof left === 'number' && typeof right === 'number') {
-    return left < right ? -1 :
-           left === right ? 0 :
-           1;
+    return left < right ? -1 
+         : left === right ? 0 
+         : 1;
   }
   else if (typeof left === 'number' && Array.isArray(right)) {
     return comparePkgPair([left], right)
@@ -17,18 +17,14 @@ const comparePkgPair = (left: any, right: any): number => {
   else if (Array.isArray(left) && Array.isArray(right)) {
     let i = 0; 
     while (i < left.length && i < right.length) {
-      const c = comparePkgPair(left[i], right[i]);
-      if (c === 1 || c === -1)
-        return c;
+      const result = comparePkgPair(left[i], right[i]);
+      if (result === 1 || result === -1)
+        return result;
       i++;
     }
-    if (i === left.length && i < right.length)
-      return -1;
-    else if (i === right.length && i < left.length) {
-      return 1;
-    }
-    else
-      return 0;
+    return (i === left.length &&  i < right.length) ? -1
+         : (i === right.length && i < left.length) ? 1
+         : 0;
   }
   else {
     return comparePkgPair(left, [right])
@@ -36,12 +32,13 @@ const comparePkgPair = (left: any, right: any): number => {
 }
 
 // Part 1
-const indicies = packetPairs.map((pair, i) => comparePkgPair(pair[0], pair[1]) === -1 ? i + 1 : 0);
-
-console.log("Part One", indicies.reduce((a, b) => a + b, 0));
+const correctIndices = packetPairs.map((pair, i) => 
+  comparePkgPair(pair[0], pair[1]) === -1 ? i + 1 : 0);
+const sumOfCorrectIndices = correctIndices.reduce((a, b) => a + b, 0)
 
 // Part 2
 const dividerPackets = [[[2]], [[6]]]
+
 const packets = packetPairs.flat()
 packets.push(...dividerPackets)
 
@@ -50,4 +47,5 @@ const dividerIndex =
   (sorted.findIndex(pkg => dividerPackets[0] === pkg) + 1) *
   (sorted.findIndex(pkg => dividerPackets[1] === pkg) + 1);
 
-  console.log("Part Two", dividerIndex)
+console.log("Part One", sumOfCorrectIndices);
+console.log("Part Two", dividerIndex)
