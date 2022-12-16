18b049284e8217423a08eae52f5917299895550a
Simon Schödler <simon.schoedler@nexplore.ch>
2022-12-17 00:19:01 +0100

    Adds Day 16 part 2

--- 2022/typescript/16/solution.ts
+++ 2022/typescript/16/solution.ts
@@ -1,9 +1,9 @@
 import * as fs from 'fs';
+
 type Valve = { 
   id: string,
   flowRate: number, 
   connections: string[],
-  state: boolean
 };
 
 type ValveNetwork = {
@@ -23,9 +23,27 @@ fs.readFileSync('./input.txt', 'utf-8').split('\r\n')
       id,
       flowRate: Number(valveMatch[2]),
       connections, 
-      state: false
     };
   });
+
+
+const memoizeMaxRel = (fn: (valve: Valve, opened: string[], minutesLeft: number, type: 'P1' | 'P2') => any) => {
+  const results: { [key:string]: any }  = {};
+  return (valve: Valve, opened: string[], minutesLeft: number, type: 'P1' | 'P2') => {
+    
+    let valveStr = valve.connections.join()
+    valveStr += valve.flowRate
+    valveStr += valve.id
+    let openedStr = opened.join();
+    
+    const argsKey = valveStr + openedStr + minutesLeft //+ type;
+
+    if (!results[argsKey]) {
+      results[argsKey] = fn(valve, opened, minutesLeft, type)
+    }
+    return results[argsKey];
+  }
+}
 
 const memoize = (fn: (...args: any) => any) => {
   const results: { [key:string]: any }  = {};
@@ -38,32 +56,36 @@ const memoize = (fn: (...args: any) => any) => {
   }
 }
 
-const maxRelief = memoize((valve: Valve, opened: string[], minutesLeft: number): number => {
-  if (minutesLeft <= 0)
-    return 0;
+const maxRelief = memoizeMaxRel((valve: Valve, opened: string[], minutesLeft: number, type: 'P1' | 'P2'): number => {
+  if (minutesLeft <= 0) {
+    return type === 'P1' ? 0 : maxRelief(valves['AA'], opened, 30-4, 'P1');
+  }
 
   const outcomes: number[] = [0];
 
   if (!opened.includes(valve.id) && valve.flowRate > 0) {
     const reliefedPressure = (minutesLeft - 1) * valve.flowRate;
     valve.connections.forEach(nextValveId => {
-      outcomes.push(reliefedPressure + maxRelief(valves[nextValveId], opened.concat(valve.id), minutesLeft - 2))
+      outcomes.push(reliefedPressure + maxRelief(valves[nextValveId], opened.concat(valve.id), minutesLeft - 2, type))
     });
   }
 
   // Unopened
   valve.connections.forEach(nextValveId => {
-    outcomes.push(maxRelief(valves[nextValveId], opened, minutesLeft - 1))
+    outcomes.push(maxRelief(valves[nextValveId], opened, minutesLeft - 1, type))
   });
 
   return Math.max(...outcomes);
 });
 
-
 let t = Date.now();
-const maxReliefedPressure = maxRelief(valves['AA'], [], 30);
+const maxReliefedPressure = maxRelief(valves['AA'], [], 30, 'P1');
 const maxReliefTimeMs = Date.now() - t;
+
+t = Date.now();
+const maxReliefedPressureWithElephant = maxRelief(valves['AA'], [], 30, 'P2');
+const maxReliefWithElephantTimeMs = Date.now() - t;
 
 // Config i7-11800H @ 2.3Ghz, 32GB RAM node v16.13.2
 console.log("Part One", maxReliefedPressure, `took ${maxReliefTimeMs}ms`); // 1460 took 142661ms
-console.log("Part Two", );
+console.log("Part Two", maxReliefedPressureWithElephant, `took ${maxReliefWithElephantTimeMs}ms`); // 2117
