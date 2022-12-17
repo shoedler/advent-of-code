a23749dd67660ccccf74dfb0052f60f3060038b0
Simon Schödler <simon.schoedler@hotmail.ch>
2022-12-17 16:44:21 +0100

    Refactors day 17 to use 28 bit numbers for shapes

--- 2022/typescript/17/solution.ts
+++ 2022/typescript/17/solution.ts
@@ -10,44 +10,80 @@ fs.readFileSync('./input.txt', 'utf-8').split('\r\n')
 const WIDTH = 7;
 const SHAPE_PAD = 3;
 
-const nextShape = (shapeIndex: number) => {
-  const shape = (shapeIndex === 0) ? [ 
-    0b0011110
-  ] : (shapeIndex === 1) ? [
-    0b0001000,
-    0b0011100,
-    0b0001000
-  ] : (shapeIndex === 2) ? [
-    0b0000100,
-    0b0000100,
-    0b0011100
-  ] : (shapeIndex === 3) ? [
-    0b0010000,
-    0b0010000,
-    0b0010000,
-    0b0010000,
-  ] : (shapeIndex === 4) ? [
-    0b0011000,
-    0b0011000
-  ] : undefined;
+const MASK_ALL = parseInt('1'.repeat(WIDTH), 2);
+const MASK_LEFTMOST = parseInt('1' + '0'.repeat(WIDTH-1), 2);
 
-  return shape.reverse();
+const SHAPE_LINE =  0b0011110;
+const SHAPE_PLUS = (0b0001000 << 14) + 
+                   (0b0011100 << 7) + 
+                    0b0001000;
+const SHAPE_L =    (0b0000100 << 14) +
+                   (0b0000100 << 7) +
+                    0b0011100;
+const SHAPE_I =    (0b0010000 << 21) +
+                   (0b0010000 << 14) +
+                   (0b0010000 << 7) +
+                    0b0010000;
+const SHAPE_SQ =   (0b0011000 << 7) +
+                    0b0011000;
+
+const shapes = [SHAPE_LINE, SHAPE_PLUS, SHAPE_L, SHAPE_I, SHAPE_SQ];
+
+const getShapeSlice = (shape: number, slice: number): number => 
+  slice > 3 ? 0 : 
+  (shape >> (slice * 7)) & MASK_ALL;
+
+const getShapeSize = (shape: number): number => 
+  shape <= MASK_ALL ? 1 :
+  shape <= (MASK_ALL << 7) ? 2 :
+  shape <= (MASK_ALL << 14) ? 3 :
+  shape <= (MASK_ALL << 21) ? 4 :
+  undefined;
+
+const printShape = (shape: number) => {
+  let sliceIndex = 0;
+  let slice = 0;
+  while ((slice = getShapeSlice(shape, sliceIndex++)) > 0) {
+    console.log(slice.toString(2).padStart(WIDTH, '0').split('').map(c => c === '1' ? '#' : '.').join(''));
+  }
 }
 
-const nudgeRight = (shape: number[]): number[] => 
-  shape.some(n => (n & 0b1) !== 0) ? shape : shape.map(n => n >> 1);
+const nudgeRight = (shape: number): number => {
+  let sliceIndex = 0;
+  let slice = 0;
+  let nudgedSlice = 0;
 
-const nudgeLeft = (shape: number[]): number[] => 
-  shape.some(n => (n & parseInt(`1${'0'.repeat(WIDTH-1)}`, 2)) !== 0) ? shape : shape.map(n => n << 1);
+  while ((slice = getShapeSlice(shape, sliceIndex)) > 0) {
+    if ((slice & 0b1) !== 0) 
+      return shape; // If any slice has its rightmost bit set, we can't nudge right
 
-const rowToString = (row: number) => 
-  row.toString(2).padStart(WIDTH, '0').split('').map(c => c === '1' ? '#' : '.').join('');
+    nudgedSlice |= (slice >> 1) << (sliceIndex * 7);
+    sliceIndex++;
+  }
 
-const rowsToString = (rows: number[]) => rows.map(rowToString).join('\n');
+  return nudgedSlice;
+}
 
-const collides = (shape: number[], shapeInChamberIndex: number, chamber: number[]) => {
-  for (let i = shapeInChamberIndex; i < (shapeInChamberIndex + shape.length); i++) {
-    const shapeSlice = shape[i - shapeInChamberIndex];
+const nudgeLeft = (shape: number): number => {
+  let sliceIndex = 0;
+  let slice = 0;
+  let nudgedSlice = 0;
+
+  while ((slice = getShapeSlice(shape, sliceIndex)) > 0) {
+    if ((slice & MASK_LEFTMOST) !== 0)  
+      return shape; // If any slice has its leftmost bit set, we can't nudge left
+
+    nudgedSlice |= (slice << 1) << (sliceIndex * 7);
+    sliceIndex++;
+  }
+
+  return nudgedSlice;
+}
+
+const collides = (shape: number, chamberIndex: number, chamber: number[]) => {
+  const shapeSize = getShapeSize(shape);
+  for (let i = chamberIndex; i < (chamberIndex + shapeSize); i++) {
+    const shapeSlice = getShapeSlice(shape, i - chamberIndex);
     const chamberSlice = chamber[i];
     
     if (i > chamber.length - 1)
@@ -58,34 +94,40 @@ const collides = (shape: number[], shapeInChamberIndex: number, chamber: number[
   return false;
 }
 
-const merge = (shape: number[], shapeInChamberIndex: number, chamber: number[]) => {
-  for (let i = shapeInChamberIndex; i < (shapeInChamberIndex + shape.length); i++) {
-    const shapeSlice = shape[i - shapeInChamberIndex];
+const merge = (shape: number, chamberIndex: number, chamber: number[]): number[] => {
+  const shapeSize = getShapeSize(shape);
+
+  for (let i = chamberIndex; i < (chamberIndex + shapeSize); i++) {
+    const shapeSlice = getShapeSlice(shape, i - chamberIndex);
     
-    if (chamber[i] === undefined)
+    if (chamber[i] === undefined) {
       chamber[i] = shapeSlice; // Extend the chamber
+    }
     else 
       chamber[i] |= shapeSlice;
   }
+
+  return chamber;
 }
 
-const simulate = (iterations: number, chamber: number[] = []) => {
+const simulate = (iterations: number) => {
   let count = 0;
   let cmdPtr = 0;
+  const chamber: number[] = []
+
   const nextCommand = () => commands[cmdPtr++ % commands.length];
   
   while (count < iterations) {
-    let shape = nextShape(count++ % 5);
+    let shape = shapes[count++ % 5];
   
     // Simulate the shape moving left and right in the defined void
     for (let i = 0; i < SHAPE_PAD;  i++) {
       shape = nextCommand() === '<' ? nudgeLeft(shape) : nudgeRight(shape);
     }
-  
+
     // Simulate the shape falling in the chamber
     let y = chamber.length;
     while (true) {
-  
       if (nextCommand() === '<') {
         const nudge = nudgeLeft(shape);
         if (!collides(nudge, y, chamber) || y === 0) {
@@ -98,9 +140,9 @@ const simulate = (iterations: number, chamber: number[] = []) => {
           shape = nudge;
         }
       }
-  
+
       y--;
-  
+
       if (collides(shape, y, chamber) || y < 0) {
         merge(shape, y+1, chamber);
         break;
@@ -116,10 +158,10 @@ let t = Date.now();
 const chamber2022Iters = simulate(2022);
 const chamber2022ItersTimeMs = Date.now() - t;
 
-console.log("Part One", chamber2022Iters.length, `took ${chamber2022ItersTimeMs}ms`); // 3098 took 27ms
+console.log("Part One", chamber2022Iters.length, `took ${chamber2022ItersTimeMs}ms`); // 3098 took 20ms, 2ms after running it 10 times consecutively
 
 t = Date.now();
 const chamber1e12Iters = simulate(1_000_000_000_000);
 const chamber1e12ItersTimeMs = Date.now() - t;
 
-console.log("Part Two", chamber1e12Iters.length, `took ${chamber1e12ItersTimeMs}ms`); // 3098 took 27ms
+console.log("Part Two", chamber1e12Iters.length, `took ${chamber1e12ItersTimeMs}ms`); // approx. 11 days 
