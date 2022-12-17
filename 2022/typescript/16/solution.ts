d758167e16a0042af05e020a644e7a6bc3aac044
Simon Schödler <simon.schoedler@hotmail.ch>
2022-12-17 23:29:52 +0100

    Refactors day 16

--- 2022/typescript/16/solution.ts
+++ 2022/typescript/16/solution.ts
@@ -1,91 +1,75 @@
 import * as fs from 'fs';
 
-type Valve = { 
-  id: string,
-  flowRate: number, 
-  connections: string[],
-};
+const Flowrates: { [key: number]: number } = {};
+const Tunnels: { [key: number]: number[] } = {};
+const ValveIds: { [key: string]: number } = {};
 
-type ValveNetwork = {
-  [key: string] : Valve
-}
-
-const valves: ValveNetwork = {};
-
+const valveDefs: { [key: string]: { numId: number, flowRate: number, connections: string[] } } = {}
 fs.readFileSync('./input.txt', 'utf-8').split('\r\n')
-  .forEach(line => {
+  .forEach((line, index) => {
     const [ left, right ] = line.split(';');
     const valveMatch = /^Valve ([A-Z]+) has flow rate=(-?\d+)/g.exec(left);
-    const id = valveMatch[1];
+    const valveLabel = valveMatch[1];
     const connections = /([A-Z]{2}(,\s)?)+/g.exec(right)[0].split(', ');
 
-    valves[id] = {
-      id,
+    valveDefs[valveLabel] = {
+      numId: index,
       flowRate: Number(valveMatch[2]),
       connections, 
     };
   });
 
+Object.entries(valveDefs).forEach(([valveLabel, valve]) => {
+  Flowrates[valve.numId] = valve.flowRate;
+  Tunnels[valve.numId] = valve.connections.map(connection => valveDefs[connection].numId);
+  ValveIds[valveLabel] = valve.numId;
+});
 
-const memoizeMaxRel = (fn: (valve: Valve, opened: string[], minutesLeft: number, type: 'P1' | 'P2') => any) => {
-  const results: { [key:string]: any }  = {};
-  return (valve: Valve, opened: string[], minutesLeft: number, type: 'P1' | 'P2') => {
-    
-    let valveStr = valve.connections.join()
-    valveStr += valve.flowRate
-    valveStr += valve.id
-    let openedStr = opened.join();
-    
-    const argsKey = valveStr + openedStr + minutesLeft //+ type;
 
-    if (!results[argsKey]) {
-      results[argsKey] = fn(valve, opened, minutesLeft, type)
+const memoize = (fn: (valve: number, opened: number[], minutesLeft: number, type: 'P1' | 'P2') => any) => {
+  const cache: { [key:string]: any }  = {};
+  return (valve: number, opened: number[], minutesLeft: number, type: 'P1' | 'P2') => {
+    const cacheKey = `${valve}|${opened.join('.')}|${minutesLeft}`
+
+    if (!cache[cacheKey]) {
+      cache[cacheKey] = fn(valve, opened, minutesLeft, type)
     }
-    return results[argsKey];
+    return cache[cacheKey];
   }
 }
 
-const memoize = (fn: (...args: any) => any) => {
-  const results: { [key:string]: any }  = {};
-  return (...args: any) => {
-    const argsKey = JSON.stringify(args);
-    if (!results[argsKey]) {
-      results[argsKey] = fn(...args)
-    }
-    return results[argsKey];
-  }
-}
 
-const maxRelief = memoizeMaxRel((valve: Valve, opened: string[], minutesLeft: number, type: 'P1' | 'P2'): number => {
+const maxRelief = memoize((valve: number, opened: number[], minutesLeft: number, type: 'P1' | 'P2'): number => {
   if (minutesLeft <= 0) {
-    return type === 'P1' ? 0 : maxRelief(valves['AA'], opened, 30-4, 'P1');
+    return type === 'P1' ? 0 : 
+      maxRelief(ValveIds['AA'], opened, 26, 'P1');
   }
 
   const outcomes: number[] = [0];
 
-  if (!opened.includes(valve.id) && valve.flowRate > 0) {
-    const reliefedPressure = (minutesLeft - 1) * valve.flowRate;
-    valve.connections.forEach(nextValveId => {
-      outcomes.push(reliefedPressure + maxRelief(valves[nextValveId], opened.concat(valve.id), minutesLeft - 2, type))
+  if (!opened.includes(valve) && Flowrates[valve] > 0) {
+    const reliefedPressure = (minutesLeft - 1) * Flowrates[valve];
+    Tunnels[valve].forEach(nextValve => {
+      outcomes.push(reliefedPressure + maxRelief(nextValve, opened.concat(valve), minutesLeft - 2, type))
     });
   }
 
   // Unopened
-  valve.connections.forEach(nextValveId => {
-    outcomes.push(maxRelief(valves[nextValveId], opened, minutesLeft - 1, type))
+  Tunnels[valve].forEach(nextValve => {
+    outcomes.push(maxRelief(nextValve, opened, minutesLeft - 1, type))
   });
 
   return Math.max(...outcomes);
 });
 
 let t = Date.now();
-const maxReliefedPressure = maxRelief(valves['AA'], [], 30, 'P1');
+const maxReliefedPressure = maxRelief(ValveIds['AA'], [], 30, 'P1');
 const maxReliefTimeMs = Date.now() - t;
 
 t = Date.now();
-const maxReliefedPressureWithElephant = maxRelief(valves['AA'], [], 30, 'P2');
+const maxReliefedPressureWithElephant = maxRelief(ValveIds['AA'], [], 26, 'P2');
 const maxReliefWithElephantTimeMs = Date.now() - t;
 
 // Config i7-11800H @ 2.3Ghz, 32GB RAM node v16.13.2
-console.log("Part One", maxReliefedPressure, `took ${maxReliefTimeMs}ms`); // 1460 took 142661ms
+console.log("Part One", maxReliefedPressure, `took ${maxReliefTimeMs}ms`); // 1460 took 89'821ms
 console.log("Part Two", maxReliefedPressureWithElephant, `took ${maxReliefWithElephantTimeMs}ms`); // 2117
