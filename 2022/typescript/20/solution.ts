875c0b30a3b192a95d79a7769f607b07fea1c755
Simon Schödler <simon.schoedler@nexplore.ch>
2022-12-20 17:12:36 +0100

    Refactors day 20

--- 2022/typescript/20/solution.ts
+++ 2022/typescript/20/solution.ts
@@ -1,73 +1,57 @@
 import * as fs from 'fs';
 
-type SourceNode = { 
-  node: Node,
-}
+type Node = { value: number };
 
-type Node = {
-  value: number,
-  origin: SourceNode,
-}
+const origins: { [key: number]: Node } = {};
+const input = fs.readFileSync('./input.txt', 'utf-8').split('\r\n');
 
-const getNodes = () => {
-  const nodes: Node[] = [];
-  const origins: readonly SourceNode[] = fs.readFileSync('./input.txt', 'utf-8').split('\r\n').map(n => { 
-    const num = Number(n);
-    const node: Node = { value: num, origin: null };
-    const origin: SourceNode = { node };
-
-    node.origin = origin;
-    nodes.push(node);
-    return origin
+const getNodes = (): Node[] => 
+  input.map((n, i) => { 
+    const value = Number(n);
+    const node: Node = { value };
+    origins[i] = node; // Overrides or creates the key with a new reference to `node`
+    return node
   });
-  
-  return { origins, nodes }
-}
 
-const findGrove = (origins: readonly SourceNode[], nodes: Node[], iters: number) => {
+const findGrove = (nodes: Node[], iters: number) => {
+  // Beware, object entries are sorted by their key. This solution only works 
+  // because we've used the natural order from the input-source as indicies / keys in 
+  // the origin obj. Alternatively you could use an array.
+  const originValues = Object.values(origins);
   for (let i = 0; i < iters; i++) {
-    origins.forEach(origin => {  
-      const index = nodes.indexOf(origin.node);
-      let targetIndex = index + origin.node.value;
+    originValues.forEach((node) => {
+      const index = nodes.indexOf(node);
+      let targetIndex = index + node.value;
     
       targetIndex = targetIndex >= nodes.length ? targetIndex % (nodes.length - 1)
         : targetIndex < 0 ? (targetIndex % (nodes.length - 1)) + nodes.length - 1
         : targetIndex;
       
-      let node = nodes.splice(index, 1)[0];
-      nodes.splice(targetIndex, 0, node);
-    })
+      let tmp = nodes.splice(index, 1)[0];
+      nodes.splice(targetIndex, 0, tmp);
+    });
   }
 
   const zeroIndex = nodes.findIndex(node => node.value === 0);
-  const grove = [
+  const groveIndicies = [
     (((1000 % nodes.length) + zeroIndex) % nodes.length),
     (((2000 % nodes.length) + zeroIndex) % nodes.length),
     (((3000 % nodes.length) + zeroIndex) % nodes.length),
   ];
 
-  return grove.map(index => nodes[index].value).reduce((a,b) => a+b);
+  return groveIndicies.map(index => nodes[index].value).reduce((a,b) => a+b);
 }
 
 // Setup: i7-1065H, 16GB RAM node v17.8.0
-(() => {
-  const t = Date.now();
-  const { origins, nodes } = getNodes();
-  
-  const grove = findGrove(origins, nodes, 1);
-  
-  const duration = Date.now() - t;
-  console.log("Part One", grove, `took ${duration}ms`); // 11123 took 38ms
+let nodes = getNodes();
+let t = Date.now();
+let grove = findGrove(nodes, 1);
+let duration = Date.now() - t;
+console.log("Part One", grove, `took ${duration}ms`); // 11123 took 34ms
 
-})();
-
-(() => {
-  const t = Date.now();
-  const { origins, nodes } = getNodes();
-  
-  nodes.map(n => n.value *= 811589153)
-  const grove = findGrove(origins, nodes, 10);
-
-  const duration = Date.now() - t;
-  console.log("Part One", grove, `took ${duration}ms`); // 4248669215955 took 283ms
-})();
+nodes = getNodes();
+t = Date.now();
+nodes.map(n => n.value *= 811589153);
+grove = findGrove(nodes, 10);
+duration = Date.now() - t;
+console.log("Part One", grove, `took ${duration}ms`); // 4248669215955 took 271ms
