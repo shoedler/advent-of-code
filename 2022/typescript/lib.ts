4f730a0235bac2ef11fd0c22a5147b4ee2981e2a
Simon Schödler <simon.schoedler@nexplore.ch>
2022-12-23 18:58:50 +0100

    Adds day 23

---
+++ 2022/typescript/lib.ts
@@ -0,0 +1,17 @@
+export class Hashmap<K, V> {
+  private hashes: { [ key: string ]: V } = {};
+  public put = (key: K, value: V) => this.hashes[JSON.stringify(key)] = value;
+  public has = (key: K): boolean => this.hashes[JSON.stringify(key)] !== undefined;
+  public get = (key: K): V => this.hashes[JSON.stringify(key)];
+  public items = (): [K, V][] => Object.entries(this.hashes).map(([k, v]) => [JSON.parse(k), v]);
+  public size = (): number => Object.keys(this.hashes).length;
+} 
+
+export class Hashset<K> {
+  private hashes: { [ key: string ]: true } = {};
+  public put = (key: K) => this.hashes[JSON.stringify(key)] = true;
+  public has = (key: K): boolean => this.hashes[JSON.stringify(key)] !== undefined;
+  public remove = (key: K): boolean => delete this.hashes[JSON.stringify(key)];
+  public items = (): K[] => Object.keys(this.hashes).map(e => JSON.parse(e));
+  public size = (): number => Object.keys(this.hashes).length;
+}
