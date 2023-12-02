7881f7b4b5b42323723c2ef7919ae8bb4fccc0e1
Simon Schödler <simon.schoedler@hotmail.ch>
2023-12-02 07:15:23 +0100

    Refactors day 1

--- 2023/typescript/1/solution.ts
+++ 2023/typescript/1/solution.ts
@@ -1,19 +1,8 @@
 import * as fs from "fs";
-import { range } from "../../../lib";
+import { range, runPart } from "../../../lib";
 console.clear();
 
 const data = fs.readFileSync("./input.txt", "utf8");
-
-const calibrationData = data
-  .split("\r\n")
-  .map((line) =>
-    line
-      .split("")
-      .filter((char) => /[0-9]/.test(char))
-      .join("")
-  )
-  .map((nums) => parseInt(nums[0] + nums[nums.length - 1], 10))
-  .reduce((a, b) => a + b, 0);
 
 const digits = {
   one: "1",
@@ -27,26 +16,40 @@ const digits = {
   nine: "9",
 };
 
-const calibrationData2 = data
-  .split("\r\n")
-  .map((line) => {
-    const chars = line.split("");
-    let nums: string[] = [];
+const partOne = () =>
+  data
+    .split("\r\n")
+    .map((line) =>
+      line
+        .split("")
+        .filter((char) => /[0-9]/.test(char))
+        .join("")
+    )
+    .map((nums) => parseInt(nums[0] + nums[nums.length - 1], 10))
+    .reduce((a, b) => a + b, 0);
 
-    for (let i = 0; i < chars.length; i++) {
-      if (/[0-9]/.test(chars[i])) nums.push(chars[i]);
-      range(3, 5).some((j) => {
-        const slice = chars.slice(i, i + j).join("");
-        if (slice in digits) {
-          nums.push(digits[slice]);
-          return true;
-        }
-      });
-    }
-    return nums.join("");
-  })
-  .map((nums) => parseInt(nums[0] + nums[nums.length - 1], 10))
-  .reduce((a, b) => a + b, 0);
+const partTwo = () =>
+  data
+    .split("\r\n")
+    .map((line) => {
+      const chars = line.split("");
+      let nums: string[] = [];
 
-console.log("Part One", calibrationData); // 54951
-console.log("Part Two", calibrationData2); // 55218
+      for (let i = 0; i < chars.length; i++) {
+        if (/[0-9]/.test(chars[i])) nums.push(chars[i]);
+        range(3, 5).some((j) => {
+          const slice = chars.slice(i, i + j).join("");
+          if (slice in digits) {
+            nums.push(digits[slice]);
+            return true;
+          }
+        });
+      }
+
+      return nums.join("");
+    })
+    .map((nums) => parseInt(nums[0] + nums[nums.length - 1], 10))
+    .reduce((a, b) => a + b, 0);
+
+runPart("One", partOne); // 54951 took 22.9453ms, allocated 3.864096MB on the heap
+runPart("Two", partTwo); // 55218 took 22.13ms, allocated 2.121632MB on the heap.
